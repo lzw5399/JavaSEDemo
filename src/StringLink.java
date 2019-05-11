4c089417294a569cb780cc858ec9df1a45b1c393
zw lin <1130120850@qq.com>
2019-05-12 02:47:45 +0800

    看到45集

--- src/StringLink.java
+++ src/StringLink.java
@@ -1,7 +1,7 @@
 public class StringLink {
   public static void main(String[] args) {
     int a = 1;
-    String b = "2";
+    String b = "3";
     var c = Integer.parseInt(b);
 
     System.out.println(c);
