17ccb38147a0830dfc80d197a807b4f424cec882
zw lin <1130120850@qq.com>
2019-05-11 23:36:50 +0800

    看到第32集

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,11 @@
+import java.math.BigDecimal;
+
+/** hello world */
 public class HelloWorld {
-    public static void main(String[] args){
-        System.out.println("Hello Java!");
-    }
+  public static void main(String[] args) {
+    var a = BigDecimal.valueOf(0.1);
+    var b = BigDecimal.valueOf(1.0 / 10.0);
+
+    System.out.println(a.equals(b));
+  }
 }
---
+++ src/SecondMain.java
@@ -0,0 +1,9 @@
+import java.math.BigDecimal;
+
+public class SecondMain {
+  public static void main(String[] args) {
+    var d = BigDecimal.valueOf(1.0);
+
+    System.out.println("12");
+  }
+}
