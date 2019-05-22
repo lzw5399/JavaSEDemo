d64fc0b3ba2123a9f6992af8a8bc46879a78b59d
zw lin <zhiwen@kooboo.cn>
2019-05-22 23:15:36 +0800

    看到148

--- src/fun/codepie/generics/TestGeneric.java
+++ src/fun/codepie/generics/TestGeneric.java
@@ -1,7 +1,15 @@
 package fun.codepie.generics;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
 public class TestGeneric {
   public static void main(String[] args) {
     var mc = new MyCollection<String>();
+
+    List<String> x = new ArrayList<>();
+
+    var y = new Vector<String>();
   }
 }
