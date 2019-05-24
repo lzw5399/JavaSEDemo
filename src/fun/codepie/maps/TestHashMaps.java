f5057ae716057024598e63ba453bdafc85e7825e
zw lin <zhiwen@kooboo.cn>
2019-05-25 01:44:42 +0800

    看到157

---
+++ src/fun/codepie/maps/TestHashMaps.java
@@ -0,0 +1,17 @@
+package fun.codepie.maps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestHashMaps {
+  public static void main(String[] args) {
+    Map<String, String> m1 = new HashMap<String, String>();
+    m1.put("key1", "value1");
+
+    var k1 = m1.get("key1");
+    
+    Integer x = 2;
+    x.get
+    System.out.println(k1);
+  }
+}
